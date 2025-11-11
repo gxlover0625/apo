94671d99a4c608f1eb7add0bcfdb476904921781
Guanbao Liang <2031808280@qq.com>
2025-11-11 10:32:26 +0800

    opro/refactor: change num_search_steps from 10 to 50

--- opro/opro/optimization/opt_utils.py
+++ opro/opro/optimization/opt_utils.py
@@ -565,7 +565,7 @@ def run_evolution(**kwargs):
       wrong_questions_from_start_counter[idx] += 1
 
   # evolution
-  num_search_steps = 10
+  num_search_steps = 50
   for i_step in range(num_search_steps):
     print(f"\n================== Step {i_step} =====================")
     if not i_step % 10:
