446fec2e38b4bc61443275ed54e1a288b7b58833
guanbao liang <2031808280@qq.com>
2026-01-08 20:41:32 +0800

    tg/feat: support agieval gaokao math

--- textgrad/textgrad/tasks/__init__.py
+++ textgrad/textgrad/tasks/__init__.py
@@ -281,6 +281,17 @@ def load_task(task_name: str, evaluation_api: EngineLM, *args, **kwargs) -> Tupl
         fn_purpose = "The runtime of string-based function that checks if the prediction is correct."
         eval_fn = StringBasedFunction(gpqa_eval_fn, function_purpose=fn_purpose)
         return train_set, val_set, test_set, eval_fn
+    
+    elif task_name == "agieval_gaokao_math":
+        from .gaokao_math import GaoKaoMath
+        from .mygpqa import gpqa_eval_fn
+        from textgrad.autograd.string_based_ops import StringBasedFunction
+        train_set = GaoKaoMath(root=kwargs.get("data_dir"), split="train")
+        val_set = GaoKaoMath(root=kwargs.get("data_dir"), split="val")
+        test_set = GaoKaoMath(root=kwargs.get("data_dir"), split="test")
+        fn_purpose = "The runtime of string-based function that checks if the prediction is correct."
+        eval_fn = StringBasedFunction(gpqa_eval_fn, function_purpose=fn_purpose)
+        return train_set, val_set, test_set, eval_fn
 
     elif task_name == "gpqa":
         from .mygpqa import GPQA, gpqa_eval_fn
