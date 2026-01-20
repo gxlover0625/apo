7c99e0c98fe66fcfe183b857d0ebb020eb6d11b4
guanbao liang <2031808280@qq.com>
2026-01-20 15:49:38 +0800

    textgrad/feat: support gaokao geography

--- textgrad/main.py
+++ textgrad/main.py
@@ -186,7 +186,7 @@ elif args.dataset == "AGIEvalMath":
 elif args.dataset in ["agieval_aqua", "agieval_gaokao_math", "agieval_sat", "math_group"]:
     default_desc = """Let's solve the problem."""
     output_format = f"Format your response as follows: \"The correct answer is (insert answer here)\""
-elif args.dataset in ["agieval_gaokao_history", "agieval_gaokao_chinese"]:
+elif args.dataset in ["agieval_gaokao_history", "agieval_gaokao_chinese", "agieval_gaokao_geography"]:
     default_desc = """Let's solve the problem."""
     output_format = f"Format your response as follows: \"The correct answer is (insert answer here)\""
 elif args.dataset == "gpqa":
