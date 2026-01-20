99ec54aa430338700a2209d5a00ccba37f9d46ad
guanbao liang <2031808280@qq.com>
2026-01-20 15:52:19 +0800

    mvp/feat: support gaokao geography

--- mvp/task.py
+++ mvp/task.py
@@ -113,6 +113,6 @@ def get_eval_fn(dataset_name:str):
         return gpqa_eval_fn
     elif dataset_name in ["agieval_aqua", "agieval_gaokao_math", "agieval_sat", "math_group"]:
         return gpqa_eval_fn
-    elif dataset_name in ["agieval_gaokao_history", "agieval_gaokao_chinese"]:
+    elif dataset_name in ["agieval_gaokao_history", "agieval_gaokao_chinese", "agieval_gaokao_geography"]:
         return gpqa_eval_fn
 
