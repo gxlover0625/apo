71da7d124cefd9b47515a6aff659d8166361e5b2
guanbao liang <2031808280@qq.com>
2026-02-28 17:12:57 +0800

    memapo/feat: 错误模式小幅更新

--- memapo/epm.py
+++ memapo/epm.py
@@ -3,6 +3,7 @@ from typing import List, Set, Dict, Any
 from uuid import uuid4
 
 from utils import get_id, get_timestamp
+from storage import VectorStore
 
 @dataclass
 class BadCase:
@@ -16,23 +17,32 @@ class ErrorPattern:
         self.idx = get_id(prefix="error_pattern")
         self.pattern = pattern
         self.bad_cases = bad_cases
-        self.metadata = metadata or {}
     
     def update(self, *args, **kwargs):
         pass
         
 class ErrorPatternMemory:
-    def __init__(self):
+    def __init__(self, restore_path:str="./db", collection_name:str=None, emb_model:str=None, threshold:float=None, topk:int=None):
         self.all_bad_cases:Set[BadCase] = set()
-        self.error_pattern_clusters = []
-        pass
+        self.error_pattern_clusters = {}
+        self.db = VectorStore(restore_path, collection_name, emb_model, threshold, topk)
 
     def add_bad_case(self, bad_case:BadCase):
         self.all_bad_cases.add(bad_case)
         # TODO
 
     def add_error_pattern(self, error_pattern:ErrorPattern):
-        pass
+        doc_id = error_pattern.idx
+        doc_content = error_pattern.pattern
+        doc_metadata = {
+            "id": doc_id,
+            "timestamp": get_timestamp(),
+            "type": "error_pattern",
+            "pattern": error_pattern.pattern,
+        }
+        self.db.add(doc_id, doc_content, doc_metadata)
+        self.error_pattern_clusters[doc_id] = error_pattern 
 
-    def retrieve(self, *args, **kwargs)->List[ErrorPattern]:
-        pass
+    def retrieve(self, question:str, *args, **kwargs)->List[ErrorPattern]:
+        # 当前只考虑最简单的实现，召回所有的error pattern
+        return list(self.error_pattern_clusters.values())
